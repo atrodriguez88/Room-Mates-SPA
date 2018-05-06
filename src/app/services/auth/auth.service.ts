05a6ba0a784b9e96b22b8fff5ad77e8ac09c3ccb
Ariel <ariel.triana88@gmail.com>
2018-05-06 10:34:19 -0400

    hadler Exception

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,21 +1,23 @@
-import { Injectable } from '@angular/core';
-import { Http, RequestOptions, Headers } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { Http, RequestOptions, Headers } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/catch";
+import "rxjs/add/operator/map";
+import "rxjs/add/observable/throw";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class AuthService {
-
-  urlBase = 'http://localhost:5000/api/account/';
+  urlBase = "http://localhost:5000/api/account/";
   token: string;
   expiration: string;
 
-  constructor(private http: Http, private router: Router) { }
+  constructor(private http: Http, private router: Router) {}
 
   getUser() {
-    return this.http.get(this.urlBase)
-      .map((res) => res.json())
+    return this.http
+      .get(this.urlBase)
+      .map(res => res.json())
       .catch(this.handleError);
   }
 
@@ -23,24 +25,24 @@ export class AuthService {
     const body = JSON.stringify(model);
     console.log(body);
 
-    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const headers = new Headers({ "Content-Type": "application/json" });
     const options = new RequestOptions({ headers: headers });
 
-    return this.http.post(`${this.urlBase}login`, body, options)
+    return this.http
+      .post(`${this.urlBase}login`, body, options)
       .map(res => {
         console.log(res);
         const user = res.json();
         if (user) {
           console.log(user);
-          localStorage.setItem('token', user.token);
-          localStorage.setItem('expiration', user.expiration);
+          localStorage.setItem("token", user.token);
+          localStorage.setItem("expiration", user.expiration);
           this.token = user.token;
           this.expiration = user.expiration;
         }
-      });
-    // .catch(this.handleError);
+      })
+      .catch(this.handleError);
   }
-
 
   logout() {
     this.token = null;
@@ -50,22 +52,32 @@ export class AuthService {
 
   register(model: any) {
     const body = JSON.stringify(model);
-    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const headers = new Headers({ "Content-Type": "application/json" });
     const options = new RequestOptions({ headers: headers });
 
     return this.http.post(`${this.urlBase}create`, body, options)
-      .map(res => {
-        const user = res.json();
-        if (user) {
-          localStorage.setItem('token', user.token);
-          localStorage.setItem('expiration', user.expiration);
-          this.token = user.token;
-          this.expiration = user.expiration;
-        }
-      });
+    .map(res => {
+      const user = res.json();
+      if (user) {
+        localStorage.setItem("token", user.token);
+        localStorage.setItem("expiration", user.expiration);
+        this.token = user.token;
+        this.expiration = user.expiration;
+      }
+    })
+    .catch(this.handleError);
   }
   private handleError(error) {
     console.error(error);
-    return Observable.throw(error.json().error || 'Server error');
+    const serveError = error.json().error;
+    let modelStateErrors = '';
+    if (serveError) {
+      for (const key in serveError) {
+        if (serveError.hasOwnProperty(key)) {
+          modelStateErrors = serveError[key];
+        }
+      }
+    }
+    return Observable.throw(modelStateErrors || 'Server error');
   }
 }
